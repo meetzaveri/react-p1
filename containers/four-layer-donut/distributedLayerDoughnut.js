bd3035e4d3c5e1e7c620b82b7a5d32ac620c52fc
Meet Zaveri <meethpatel96@gmail.com>
2018-01-23 09:30:44 +0530

    Update distributedLayerDoughnut.js

--- containers/four-layer-donut/distributedLayerDoughnut.js
+++ containers/four-layer-donut/distributedLayerDoughnut.js
@@ -6,20 +6,17 @@ import Pie from '../../components/pie';
 
 const distributedLayerDoughnut = () => (
     <div style={{position : 'relative'}}> 
-        <Slice  slicedegree="36">
-            <Pie piebg="yellow" degree="252" />
+        <Slice  slicedegree="0">
+            <Pie piebg="yellow" degree="270" />
         </Slice>
-        <Slice clipnative slicedegree="72">
-            <Pie clipnative piebg="red" degree="252" />
+        <Slice clipnative slicedegree="90">
+            <Pie clipnative piebg="red" degree="270" />
         </Slice>
-        <Slice clipnative slicedegree="144" >
-            <Pie clipnative piebg="pink" degree="252" />
+        <Slice clipnative slicedegree="180" >
+            <Pie clipnative piebg="pink" degree="270" />
         </Slice>
-        <Slice clipnative slicedegree="216">
-            <Pie clipnative piebg="blue" degree="252" />
-        </Slice>
-        <Slice clipnative slicedegree="288">
-            <Pie clipnative piebg="green" degree="252" />
+        <Slice clipnative slicedegree="270">
+            <Pie clipnative piebg="blue" degree="270" />
         </Slice>
     </div>
 )

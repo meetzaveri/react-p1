124bbdf266cbf6fced53518b1734d1f911ec18ed
Meet Zaveri <meethpatel96@gmail.com>
2018-01-23 09:24:32 +0530

    Update index.js

--- containers/five-layer-donut/index.js
+++ containers/five-layer-donut/index.js
@@ -4,7 +4,7 @@ import Circle from './circle';
 class Main extends React.Component{
     render(){
         return(
-            <Circle hasPrimary shouldRemainEqual={true} pievalues={{p1:5,p2:5,p3:35,p4:40,p5:15}} />
+            <Circle hasPrimary shouldRemainEqual={true} pievalues={{p1:5,p2:25,p3:20,p4:40,p5:10}} />
         );
     }
 }
